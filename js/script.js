ac99d1b91d107a20591cee3e1fb424291d48f6b5
diogopaes1 <diogopaes1@gmail.com>
2025-09-06 00:25:38 -0300

    Finalização: Correção definitiva de todos os arquivos e atualização da documentação

--- js/script.js
+++ js/script.js
@@ -53,23 +53,27 @@ document.addEventListener('DOMContentLoaded', function() {
         const burger = document.querySelector('.burger');
         const nav = document.querySelector('.nav-links');
         const navLinks = document.querySelectorAll('.nav-links li');
+        const body = document.querySelector('body');
 
-        burger.addEventListener('click', () => {
-            // Toggle Nav
-            nav.classList.toggle('nav-active');
+        if (burger) {
+            burger.addEventListener('click', () => {
+                // Toggle Nav
+                nav.classList.toggle('nav-active');
+                body.classList.toggle('no-scroll'); // Trava/Destrava o scroll
 
-            // Animate Links
-            navLinks.forEach((link, index) => {
-                if (link.style.animation) {
-                    link.style.animation = '';
-                } else {
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-                }
+                // Animate Links
+                navLinks.forEach((link, index) => {
+                    if (link.style.animation) {
+                        link.style.animation = '';
+                    } else {
+                        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                    }
+                });
+
+                // Burger Animation
+                burger.classList.toggle('toggle');
             });
-
-            // Burger Animation
-            burger.classList.toggle('toggle');
-        });
+        }
     }
 
     navSlide();
